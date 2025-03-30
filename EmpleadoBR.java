06b9ac6770daf3223ef8bca7c77ed90f73f07989
Santi03fs <santifernandez652@gmail.com>
2025-03-31 00:31:03 +0200

    Add files via upload

---
+++ EmpleadoBR.java
@@ -0,0 +1,48 @@
+package es.iesmz;
+
+public class EmpleadoBR {
+
+    public static float calculaSalarioBruto(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+        if (tipo == null || ventasMes < 0 || horasExtra < 0) {
+            return -1;
+        }
+        float salarioBase;
+        if (tipo == TipoEmpleado.venedor) {
+            salarioBase = 1000;
+        } else {
+            salarioBase = 1500;
+        }
+
+        float prima;
+        if (ventasMes >= 1500) {
+            prima = 200;
+        } else if (ventasMes >= 1000) {
+            prima = 100;
+        } else {
+            prima = 0;
+        }
+
+        if(horasExtra > 0){
+            salarioBase += (horasExtra * 20);
+        }
+
+        return salarioBase + prima ;
+    }
+
+    public static float calculaSalarioNeto(float salarioBruto) {
+        if (salarioBruto < 0) {
+            return -1;
+        }
+        if(salarioBruto < 1000){
+            return salarioBruto;
+        }
+        float retencion;
+        if (salarioBruto < 1500) {
+            retencion = 0.16f;
+        } else {
+            retencion = 0.18f;
+        }
+
+        return salarioBruto * (1 - retencion);
+    }
+}
---
+++ EmpleadoBRTest.java
@@ -0,0 +1,136 @@
+package es.iesmz;
+
+import org.junit.jupiter.api.BeforeEach;
+import  org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+ class EmpleadoBRTest {
+
+     @BeforeEach
+     public void before(){
+         System.out.println("EXECUTE PROVA");
+     }
+
+    @Test
+    public void testCalculaSalarioNeto1(){
+        assertEquals(1640,EmpleadoBR.calculaSalarioNeto(2000));
+    }
+
+    @Test
+     public void testCalculaSalarioNeto2(){
+        assertEquals(1230,EmpleadoBR.calculaSalarioNeto(1500));
+    }
+
+    @Test
+     public void testCalculaSalarioNeto3(){
+        assertEquals(1259.9916f,EmpleadoBR.calculaSalarioNeto(1499.99f), 0.001f);
+    }
+
+     @Test
+     public void testCalculaSalarioNeto4(){
+         assertEquals(1050, EmpleadoBR.calculaSalarioNeto(1250));
+     }
+
+    @Test
+     public void testCalculaSalarioNeto5(){
+        assertEquals(840, EmpleadoBR.calculaSalarioNeto(1000), 0.01f);
+    }
+
+     @Test
+     public void testCalculaSalarioNeto6(){
+         assertEquals(999.0, EmpleadoBR.calculaSalarioNeto(999), 0.1);
+     }
+
+
+     @Test
+     public void testCalculaSalarioNeto7(){
+         assertEquals(500, EmpleadoBR.calculaSalarioNeto(500));
+     }
+
+
+     @Test
+     public void testCalculaSalarioNeto8(){
+         assertEquals(0, EmpleadoBR.calculaSalarioNeto(0));
+     }
+
+
+     @Test
+     public void testCalculaSalarioNeto9(){
+         assertEquals(-1, EmpleadoBR.calculaSalarioNeto(-1));
+     }
+
+     @Test
+     public void calculaSalarioBruto1(){
+        assertEquals(1360, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.venedor, 2000, 8));
+     }
+
+     @Test
+     public void calculaSalarioBruto2(){
+         assertEquals(1260, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.venedor, 1500, 3));
+     }
+
+     @Test
+     public void calculaSalarioBruto3(){
+         assertEquals(1110, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.venedor, 1499.99f, 0.5f), 0.0010f);
+     }
+
+     @Test
+     public void calculaSalarioBruto4(){
+         assertEquals(1760, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encaarregat, 1250, 8));
+     }
+
+     @Test
+     public void calculaSalarioBruto5(){
+         assertEquals(1600, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encaarregat, 1000, 0));
+     }
+
+     @Test
+     public void calculaSalarioBruto6(){
+         assertEquals(1560, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encaarregat, 999.99f, 3), 0.0010f);
+     }
+
+
+     @Test
+     public void calculaSalarioBruto7(){
+         assertEquals(1500, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encaarregat, 500, 0));
+     }
+
+     @Test
+     public void calculaSalarioBruto8(){
+         assertEquals(1660, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.encaarregat, 0, 8));
+     }
+
+     @Test
+     public void calculaSalarioBruto9(){
+         assertEquals(-1, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.venedor, -1, 8));
+     }
+
+     @Test
+     public void calculaSalarioBruto10(){
+         assertEquals(-1, EmpleadoBR.calculaSalarioBruto(TipoEmpleado.venedor, 1500, -1));
+     }
+
+     @Test
+     public void calculaSalarioBruto11(){
+         assertEquals(-1, EmpleadoBR.calculaSalarioBruto(null , 1500, 8));
+     }
+
+     @Test
+     public void calculaSalarioBruto12(){
+         assertEquals(-1,EmpleadoBR.calculaSalarioBruto(null,4500,4));
+     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
